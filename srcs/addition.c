940916c6e89aa0068b97329f28bd851d82b977c7
Danilov Dimitri <dimitri.danilov@epitech.eu>
2014-10-31 19:07:27 +0100

    addition plus propore

--- srcs/addition.c
+++ srcs/addition.c
@@ -15,21 +15,23 @@ char			*addition(char *str_1, char *str_2)
     }
   str3 = malloc(sizeof(char) * add.taille_2 + 2);
   add.position = add.taille_2;
-  str3[add.position] = '\0';
+  str3[add.position + 1] = '\0';
   if (str3 == NULL)
     return (NULL);
-  while(add.taille_1 >= 0 && add.taille_2 >= 0)
+  while(add.taille_1 >= 1 && add.taille_2 >= 1)
     {
       str3[add.position] = str_1[add.taille_1 - 1] + str_2[add.taille_2 - 1]- 48;
-      if (str3[add.position] >= 58)
-        {
-          str_1[add.taille_1 - 1]= str_1[add.taille_1 - 1] + 1;
-          str3[add.position] = str3[add.position] - 10;
-        }
+       if (str3[add.position] >= 58) 
+         {
+           str_1[add.taille_1 - 2]= str_1[add.taille_1 - 2] + 1; 
+           str3[add.position] = str3[add.position] - 10; 
+         } 
       add.position = add.position - 1;
       add.taille_1 = add.taille_1 - 1;
       add.taille_2 = add.taille_2 - 1;
     }
+  str3[0] = '+';
+  str3[1] = 'a';
   return (str3);
 }
 
